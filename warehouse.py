9bf491efebd6d0c3680664f228f993be59b3b608
Vangel Paronov <vparonov@gmail.com>
2023-03-21 21:15:54 +0100

    checkpoint

--- warehouse.py
+++ warehouse.py
@@ -1,12 +1,177 @@
 import json
 import random 
+import numpy as np
 
 from conveyor import Conveyor
 from diverter import Diverter, DIVERT, CONTINUE_STRAIGHT
 from agent import Agent
 from sink import Sink
+from source import Source, FIFO, SKIP
+from box import BoxListFromFile
 
 
+class Warehouse:
+    def __init__(self, name, fileName, datadir, randomFileSelect = False):
+        self.name = name
+        self.randomFileSelect = randomFileSelect
+        if datadir != None:
+            self.datafiles = self.enumerateDataFiles(datadir)
+            random.shuffle(self.datafiles)
+            self.fileIx = 0
+        else:
+            self.datafiles = []
+        
+        self.action_space = ActionSpace([SKIP, FIFO])
+        self.build(fileName)
+
+    def build(self, fileName):
+        with open(fileName) as f:
+            data = json.load(f)
+        components = {} 
+
+        components['source'] =  (Source(name='source', values = [], sourceFn = ActionSpace([SKIP, FIFO])), 'source', -1)
+        components['sink'] = (Sink('sink'), 'sink', 999999999)
+        
+        self.stateSize = 2 
+
+        for c in data['components']:
+            type = c['type']
+            name = c['name']
+            ord = c['ord']
+            if name == 'source' or name == 'sink':
+                raise Exception('source and sink are reserved names')
+            
+            if type == 'conveyor':
+                capacity = c['capacity'] 
+                newComponent = Conveyor(name = name, 
+                                        capacity = capacity, 
+                                        delay = c['delay'])
+                self.stateSize += capacity
+            elif type == 'diverter':
+                newComponent = Diverter(name = name, 
+                                        delay = c['delay'], 
+                                        selectorFn = makeDiverterSelectorFn(c['divertStation']))
+                self.stateSize += 1
+            elif type == 'agent':
+                newComponent = Agent(name = name, 
+                                    delayFn = makeAgentDelayFn(c['minDelay'], c['maxDelay']), 
+                                    actionFn= makeAgentMarkPickedFn(c['markPicked']))
+                self.stateSize += 1
+            else:
+                print('Unknown component type:'+ type)
+
+            components[c['name']] = (newComponent, type, ord)
+
+        for conn in data['connections']:
+            component, type, _ = components[conn['from']]
+            if type == 'conveyor':
+                to, _, _ = components[conn['to']]
+                component.Connect(to.FirstPlace())
+            elif type == 'diverter':
+                to, _, _ = components[conn['to']]
+                divertTo, _, _ = components[conn['divertTo']]
+                component.Connect(to.FirstPlace(), divertTo.FirstPlace())
+            elif type == 'agent':
+                source, _, _  = components[conn['source']]
+                returnTo, _, _ = components[conn['returnTo']]
+                component.Connect(source, returnTo)
+            elif type == 'source':
+                to, _, _ = components[conn['to']]
+                component.Connect(to.FirstPlace())
+            else:
+                print('Connections. Unknown component type:'+ type)
+
+        self.componentsList = [c[1][0] for c in sorted(components.items(), key=lambda cc: cc[1][-1])]
+        self.components = components
+    
+    def getAgentsWaitingRatio(self):
+        raise Exception('Not implemented yet')
+    
+    def reward(self, state, terminated, truncated):
+        #return self.reward_min_processing_time(state, terminated, truncated)
+        return self.reward_min_total_time(state, terminated, truncated)
+    
+    def reward_min_total_time(self, state, terminated, truncated):
+        if terminated:
+            avgTotalProcessingTime =  self.t / self.components['sink'].State()[0]
+            if avgTotalProcessingTime > 0:
+                return 5.0/avgTotalProcessingTime
+            else:
+                return 0.0
+        elif truncated:
+            return 0.0
+        elif self.t > 0:
+            return -0.04 * self.getAgentsWaitingRatio()
+        else:
+            return 0.0
+        
+    def reset(self,itemsToPick = None):
+
+        if itemsToPick == None:
+            fileName = self.sampleDataFiles()
+            print(fileName)
+            itemsToPick = BoxListFromFile(fileName)
+            if random.random() > 0.8:
+                sort = random.randint(0, 2)
+                if sort == 0 :
+                    itemsToPick.sort(reverse=False, key=lambda b: b.route)
+                elif sort == 1: 
+                    itemsToPick.sort(reverse=True, key=lambda b: 1 if b.route == 2 else 0 )
+                else:
+                    itemsToPick.sort(reverse=True, key=lambda b: b.route)
+
+        for c in self.componentsList:
+            c.Reset()
+
+        for item in itemsToPick:
+            item.Reset()
+
+        state = self.getState() 
+        self.t = 0
+        self.maxT = 20000#self.calcMaxT(itemsToPick)
+        self.nitems= len(itemsToPick)
+        self.strategy.setItems(itemsToPick) 
+        return state, self.nitems, self.strategy.getActionsMask() 
+
+    def getState(self):
+        states = np.sum(self.componentsList[0].State()) / self.componentsList[0].Capacity()
+        for c in self.componentsList[1:]:
+            states = np.hstack((states, np.sum(c.State()) / c.Capacity()))
+        return states
+        
+# helper classes 
+class ActionSpace():
+    def __init__(self, actions):
+        self.actions = actions 
+        self.n = len(self.actions)
+    
+    def sample(self):
+        return random.sample(self.actions, k=1)[0]
+    
+class ActionStrategy:
+    def __init__(self):
+        self.ix = 0 
+        self.remaining_items = 0
+  
+    # def setItems(self, items):
+    #     self.ix = 0 
+    #     self.items= items
+    #     self.remaining_items = len(self.items)
+
+    def setAction(self, action):
+        self.action = action 
+
+    def getActionsMask(self):    
+        # no masked actions 
+        return [True, True]
+
+    def getEmptyActionsMask(self):
+        return [False, False]
+
+    def __call__(self, ctime):
+        return self.action 
+
+# helper functions
 def makeDiverterSelectorFn(station):
     def fn(box):
         return DIVERT if box.IsForStation(station) else CONTINUE_STRAIGHT
@@ -22,47 +187,12 @@ def makeAgentMarkPickedFn(station):
         return box.Pick(station)
     return fn
 
-with open('configuraions/wh.json') as f:
-    data = json.load(f)
 
-components = {} 
 
-components['sink'] = ('sink', Sink('sink'))
+if __name__ == '__main__':
+    wh = Warehouse('test', 'configurations/wh.json', None, False)
+    s = wh.getState()
+    print(s)
 
-for c in data['components']:
-    type = c['type']
-    if type == 'conveyor':
-        newComponent = Conveyor(name = c['name'], 
-                                capacity = c['capacity'], 
-                                delay = c['delay'])
-    elif type == 'diverter':
-        newComponent = Diverter(name = c['name'], 
-                                delay = c['delay'], 
-                                selectorFn = makeDiverterSelectorFn(c['divertStation']))
-    elif type == 'agent':
-        newComponent = Agent(name = c['name'], 
-                             delayFn = makeAgentDelayFn(c['minDelay'], c['maxDelay']), 
-                             actionFn= makeAgentMarkPickedFn(c['markPicked']))
-    else:
-        print('Unknown component type:'+ type)
 
-    components[c['name']] = (type, newComponent)
-
-for conn in data['connections']:
-    type, component = components[conn['from']]
-    if type == 'conveyor':
-        _, to = components[conn['to']]
-        component.Connect(to.FirstPlace())
-    elif type == 'diverter':
-        _, to = components[conn['to']]
-        _, divertTo = components[conn['divertTo']]
-        component.Connect(to.FirstPlace(), divertTo.FirstPlace())
-    elif type == 'agent':
-        _, source = components[conn['source']]
-        _, returnTo = components[conn['returnTo']]
-        component.Connect(source, returnTo)
-    else:
-        print('Connections. Unknown component type:'+ type)
-
-    
     
